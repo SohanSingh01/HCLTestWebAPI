9cbd2712395fa37229d68ab68a6863af594c761f
sohanrgi <sohanrgi@gmail.com>
2021-05-26 22:18:04 +0530

    Add project files.

---
+++ HCLMVCTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using HCLMVCTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HCLMVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult PostNumberSeries(SeriesModel objSeriesModel)
+        {
+            HttpClient objHttpClient = new HttpClient();
+            var result = objHttpClient.GetStringAsync("http://localhost:58689/weatherforecast?number=" + objSeriesModel.number).ToString();
+
+            return View("Index", result);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HCLMVCTest/Models/SeriesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCLMVCTest.Models
+{
+    public class SeriesModel
+    {
+       public int Id { get; set; }
+        public int number { get; set; }
+    }
+}
